30979965b6f1942da3be5817a9b1d616c6c2a972
Artur Burtsev <artjock@gmail.com>
2013-08-11 22:02:22 +0000

    api tests

--- client/js/index.js
+++ client/js/index.js
@@ -1,2 +1,15 @@
 /* borschik:include:../bower_components/underscore/underscore.js */
 /* borschik:include:../bower_components/backbone/backbone.js */
+
+
+
+$.ajax({
+    url: '/api/timers/520803b007cb9d9a5e000001',
+    method: 'delete',
+    data: {
+        name: 'Stalin'
+    },
+    complete: function() {
+        console.log(arguments);
+    }
+})
