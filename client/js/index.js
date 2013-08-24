a2b32fdae29c104be45a6c045df35d30a4c0f259
Artur Burtsev <artjock@gmail.com>
2013-08-24 15:19:11 +0000

    angular over bower

--- client/js/index.js
+++ client/js/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* borschik:include:../bower_components/angular/angular.js */
+
 /* borschik:include:app.js */
 /* borschik:include:filters.js */
 /* borschik:include:controllers.js */
