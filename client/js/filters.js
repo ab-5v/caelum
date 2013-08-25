33ee11081a79c8e96f62ca057a03b2ec4cba26f5
Artur Burtsev <artjock@gmail.com>
2013-08-25 12:34:43 +0000

    round timer values

--- client/js/filters.js
+++ client/js/filters.js
@@ -8,8 +8,8 @@ angular.module('caelum.filters', [])
                 return '00:00:00'
             }
 
-            var h = Math.floor( ts/(60*60*1000) );
-            var m = Math.floor( ts/(60*1000) );
+            var h = Math.floor( ts/(60*60*1000) ) % 24;
+            var m = Math.floor( ts/(60*1000) ) % 60;
             var s = Math.floor( ts/(1000) ) % 60;
 
             if (h < 10) { h = '0' + h; }
