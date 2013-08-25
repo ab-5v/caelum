fef0548c826cd2c97cd69f9cc5b41761e0abe001
Artur Burtsev <artjock@gmail.com>
2013-08-25 09:01:17 +0000

    client states switching

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,9 +1,18 @@
 ;(function() {
 
-angular.module('caelum.controllers', [])
-    .controller('TimersCtrl', ['$scope', '$http', TimersCtrl])
-    .controller('TimerCtrl', ['$scope', '$http', TimerCtrl])
+var API_TIMERS = '/api/timers/';
 
+var STATE = {
+    '1': 'runned',
+    '2': 'paused',
+    '3': 'zeroed',
+    'runned': 1,
+    'paused': 2,
+    'zeroed': 3
+};
+
+angular.module('caelum.controllers', [])
+    .controller('TimersCtrl', ['$scope', '$http', TimersCtrl]);
 
 function TimersCtrl($scope, $http) {
 
@@ -12,22 +21,64 @@ function TimersCtrl($scope, $http) {
         console.log('TimersCtrl.titleChange', $scope);
     };
 
-    $http.get('/api/timers/')
+    angular.extend($scope, {
+
+        updatingState: false,
+
+        create: function() {
+            $http.post(API_TIMERS)
+                .success(function(data) {
+                    $scope.setLastState(data);
+                    $scope.timers.push(data);
+                }).error(httperror);
+        },
+
+        updateTitle: function(id, title) {
+            $http.put(API_TIMERS + id, {title: title})
+                .success(function() {
+                    console.log(arguments);
+                }).error(httperror);
+        },
+
+        updateState: function(id, from) {
+            var state = from === 'runned' ? 'paused' : 'runned';
+
+            $scope.updatingState = true;
+
+            $http.put(API_TIMERS + id, {state: STATE[state]})
+                .success(function(data) {
+
+                    $scope.timers.forEach(function(timer) {
+                        if (timer._id === id) {
+                            timer.state = data.state
+                            $scope.setLastState(timer);
+                        }
+                    });
+
+                    $scope.updatingState = false;
+
+                }).error(httperror);
+        },
+
+        setLastState: function(timer) {
+            var last = timer.state[timer.state.length - 1];
+            timer.lastState = STATE[ String(last.st) ];
+        }
+
+    });
+
+    $http.get(API_TIMERS)
         .success(function(data) {
+            $scope.loaded = true;
             $scope.timers = data;
-        })
-        .error(function() {
-            console.log(arguments);
-        });
+
+            data.forEach($scope.setLastState);
+
+        }).error(httperror);
 }
 
-function TimerCtrl($scope) {
-    angular.extend($scope, $scope.$parent.timer);
-    console.log('TimerCtrl', $scope);
-
-    $scope.titleChange = function() {
-        console.log('TimerCtrl.titleChange', $scope);
-    };
-}
+function httperror() {
+    console.log(arguments);
+};
 
 })();
